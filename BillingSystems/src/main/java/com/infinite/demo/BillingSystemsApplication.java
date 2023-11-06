4400a809723c3c665f8ae153c8e92dce89d663ee
Pradeep7726 <144919206+Pradeep7726@users.noreply.github.com>
2023-11-06 09:51:40 +0530

    Add files via upload

---
+++ BillingSystems/src/main/java/com/infinite/demo/BillingSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.infinite.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BillingSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingSystemsApplication.class, args);	  
+	}
+}
